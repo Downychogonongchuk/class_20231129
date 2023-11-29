d25c64a7139adcedbcee89b9ce5a50f3fdd3bd66
Civilian <renounce774@naver.com>
2023-11-29 15:39:32 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,14 @@
+package a;
+
+public class First {
+
+	// main 메서드 (프로그램의 시작 지점)
+	public static void main(String[] args) {
+		// main 메서드 블록의 시작
+		// 실행할 내용이 작성되는 공간
+		System.out.println("Hey 자바 코딩해줘");
+		
+	
+	}// main 메서드 블록의 끝
+	
+}
